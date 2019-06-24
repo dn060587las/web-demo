9902c9cbab0d734228a0072955dccfdde3c76dc3
KirienkoEvgeniy <kirienko.evgeny@gmail.com>
2019-06-24 23:32:51 +0300

    подключить slf4j, logback в зависимости проекта spring web demo настроить логирование в проекте - убрать вызовы System.out перейти на Logger

--- src/main/java/org/demo/web/MainController.java
+++ src/main/java/org/demo/web/MainController.java
@@ -1,23 +1,26 @@
 package main.java.org.demo.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.List;
-
 @Controller
 @RequestMapping("main")
 public class MainController {
+    private static final Logger logger = LoggerFactory.getLogger(MainController.class);
 
     public MainController() {
-        System.out.println("Controller created");
+        logger.info("Controller created");
+//        System.out.println("Controller created");
     }
 
     @RequestMapping("get-some-data")
     @ResponseBody
     public String[] getSomeData() {
-        System.out.println("Controller method invoked");
+        logger.info("Controller method invoked");
+//        System.out.println("Controller method invoked");
         return new String[]{"1111", "2222", "333"};
     }
 }
