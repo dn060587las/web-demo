dd4b50a18a4c44a68a707ec0aa39852b4b0d6728
alitvinov <alexlitvinovpost@gmail.com>
2019-07-14 09:50:00 +0300

    added h2 datasource.

--- src/main/java/org/demo/components/RandomNumberComponent.java
+++ src/main/java/org/demo/components/RandomNumberComponent.java
@@ -4,12 +4,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Random;
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @Component
 public class RandomNumberComponent {
 
     private final DataStore ds;
+    
+    @Autowired
+    private JdbcTemplate dds ;
 
+    @PostConstruct
+    public void init() {
+        System.out.println("11111");
+        dds.execute("select * from test");
+        System.out.println("222");
+    }
+    
     @Autowired
     public RandomNumberComponent(DataStore ds) {
         this.ds = ds;
